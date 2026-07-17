140227ba76fbf218bf563cb50ade4a0b12e8be02
MAIN\Fitor Avdiji <favdiji@googlemail.com>
2026-07-17 10:03:14 +0200

    added routing

--- frontend/src/components/feature_panel/feature_panel.jsx
+++ frontend/src/components/feature_panel/feature_panel.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import classes from "./feature_panel.module.css";
 
+import home_icon from "../../assets/icons/home.svg";
 import calendar_icon from "../../assets/icons/calendar.svg";
 import shopping_cart_icon from "../../assets/icons/shopping_cart.svg";
+import todo_list_icon from "../../assets/icons/todo_list.svg";
 import IconButton from "../buttons/icon_button";
+import {
+  CALENDAR_PATH,
+  DASHBOARD_PATH,
+  SHOPPING_PATH,
+  TODO_PATH,
 
-const FEATURES = [
-  { title: "Home", src: calendar_icon },
-  { title: "Calendar", src: calendar_icon },
-  { title: "Shopping Lists", src: shopping_cart_icon },
-  { title: "Todos", src: calendar_icon },
-];
+  FEATURES,
+} from "../../core/constants";
+import { useNavigate } from "react-router-dom";
 
 export default function FeaturePanel() {
   const [selected, setSelected] = useState(0);
+  const nav = useNavigate();
+
+  const onClick = (index, target_path) => {
+    setSelected(index);
+    nav(target_path);
+  };
 
   return (
     <nav className={classes.feature_panel}>
@@ -23,7 +33,7 @@ export default function FeaturePanel() {
           src={f.src}
           title={f.title}
           active={selected === i}
-          onClick={() => setSelected(i)}
+          onClick={() => onClick(i, f.path)}
         />
       ))}
     </nav>
